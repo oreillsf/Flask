228bb3c6f19688764de977bd5fe69af59720dfc0
oreillsf <oreillsf@tcd.ie>
2022-07-06 22:13:31 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,349 @@
+
+
+from flask import Flask, render_template, redirect, request
+# from application import app
+# from application.models import Data
+import db_create as db 
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/returnToIndex')
+def returnToIndex():
+    return render_template('index.html')
+
+@app.route('/pinSet')
+def pinSet():
+    return render_template('pin.html')
+
+@app.route('/musicalSet')
+def musicalSet():
+    return render_template('musical.html')
+
+@app.route('/graphicalSet')
+def graphicalSet():
+    return render_template('graphical.html')
+
+@app.route('/informedConsentPage')
+def informedConsentPage():
+    return render_template('informedConsent.html')
+
+
+@app.route('/surveyPage')
+def surveyPage():
+    return render_template('survey.html')
+
+
+@app.route('/consentPageForm', methods=['POST'])
+def consentPageForm():
+    choice1 = request.form['choice1']
+    choice2 = request.form['choice2']
+    choice3 = request.form['choice3']
+    choice4 = request.form['choice4']
+    choice5 = request.form['choice5']
+    choice6 = request.form['choice6']
+
+    if request.method == 'POST':
+            db.insert_details(choice1,choice2,choice3,choice4,choice5,choice6)
+
+    return render_template('questions.html')
+
+
+@app.route('/questionForm', methods=['POST'])
+def questionForm():
+    ansQ1 = request.form['q1']
+    ansQ2 = request.form['q2']
+    ansQ3 = request.form['q3']
+    ansQ4 = request.form['q4']
+    ansQ5a = request.form['q5a']
+    ansQ5b = request.form['q5b']
+    ansQ5c = request.form['q5c']
+    ansQ5d = request.form['q5d']
+    ansQComm5a = request.form['pinComment5a']
+    ansQComm5b = request.form['pinComment5b']
+    ansQComm5c = request.form['pinComment5c']
+    ansQComm5d = request.form['pinComment5d']
+    ansQ6 = request.form['q6']    
+    ansQ7 = request.form['q7']
+    ansQ6Comm = request.form['pinComment6']
+    ansQ7Comm = request.form['pinComment7']
+
+
+    #insert all the data to db
+    if request.method == 'POST':
+            db.insert_questdetails(ansQ1,ansQ2,ansQ3,ansQ4,ansQ5a,ansQ5b,ansQ5c,ansQ5d,ansQComm5a,ansQComm5b,ansQComm5c,ansQComm5d,ansQ6,ansQ7,ansQ6Comm,ansQ7Comm)
+    return render_template('passwords.html')
+
+@app.route('/passwordForm1', methods=['POST'])
+def passwordForm1():
+    username = request.form['username']
+    password = request.form['password']
+    print("username : " + username +"\n Password "+ password)
+    #insert this data to db   
+    if request.method == 'POST':
+            db.insert_passdetails(username, password) 
+    return render_template('passwordsLogin.html')
+
+@app.route('/passwordLoginForm1', methods=['GET','POST'])
+def passwordLoginForm1():
+    username = request.form['username']
+    password = request.form['password']
+    attempt =1
+    # print("username : " + username +"\n Password "+ password)
+    exists = db.get_passdetails(username,password)
+    print(exists)
+    if exists == 'true':
+            db.insert_passsuccess('yes','na','na')
+            return render_template('pin.html') 
+    else:
+            return render_template('passwordsLogin2.html')
+
+@app.route('/passwordLoginForm2', methods=['POST'])
+def passwordLoginForm2():
+    username = request.form['username']
+    password = request.form['password']
+    attempt =2
+    print("username : " + username +"\n Password "+ password)
+    exists = db.get_passdetails(username,password)
+    print(exists)
+    if exists == 'true':
+            db.insert_passsuccess('no','yes','na')
+            return render_template('pin.html') 
+    else:
+            return render_template('passwordsLogin3.html')
+    #insert this data to db   
+    #cross verify if data is correct with db. if so go to return render_template('pin.html') else go to below
+    return render_template('passwordsLogin3.html')
+
+@app.route('/passwordLoginForm3', methods=['POST'])
+def passwordLoginForm3():
+    username = request.form['username']
+    password = request.form['password']
+    attempt =3
+    print("username : " + username +"\n Password "+ password)
+    exists = db.get_passdetails(username,password)
+    print(exists)
+    if exists == 'true':
+            db.insert_passsuccess('no','no','yes')
+            return render_template('pin.html') 
+    else:
+            db.insert_passsuccess('no','no','no')
+            return render_template('pin.html')
+    #insert this data to db   
+    return render_template('pin.html') 
+
+@app.route('/pinForm1', methods=['POST'])
+def pinForm1():
+    username = request.form['username']
+    pin = request.form['pin']
+    print("username : " + username +"\n Password "+ pin)
+    #insert this data to db   
+    if request.method == 'POST':
+        db.insert_pindetails(username, pin) 
+    return render_template('pinLogin.html')
+
+@app.route('/pinLoginForm1', methods=['POST'])
+def pinLoginForm1():
+    username = request.form['username']
+    pin = request.form['pin']
+    attempt =1
+    print("username : " + username +"\n Password "+ pin)
+    exists = db.get_pindetails(username,pin)
+    print(exists)
+    if exists == 'true':
+            db.insert_pinsuccess('yes','na','na')
+            return render_template('graphical.html') 
+    else:
+            return render_template('pinLogin2.html')
+    #insert this data to db   
+    #cross verify if data is correct with db. if so go to return render_template('graphical.html') else go to below
+
+@app.route('/pinLoginForm2', methods=['POST'])
+def pinLoginForm2():
+    username = request.form['username']
+    pin = request.form['pin']
+    attempt =2
+    print("username : " + username +"\n Password "+ pin)
+
+    exists = db.get_pindetails(username,pin)
+    print(exists)
+    if exists == 'true':
+            db.insert_pinsuccess('no','yes','na')
+            return render_template('graphical.html') 
+    else:
+            return render_template('pinLogin3.html')
+    #insert this data to db   
+    #cross verify if data is correct with db. if so go to return render_template('graphical.html') else go to below
+
+@app.route('/pinLoginForm3', methods=['POST'])
+def pinLoginForm3():
+    username = request.form['username']
+    pin = request.form['pin']
+    attempt =3
+    print("username : " + username +"\n Password "+ pin)
+    exists = db.get_pindetails(username,pin)
+    print(exists)
+    if exists == 'true':
+            db.insert_pinsuccess('no','no','yes')
+            return render_template('graphical.html') 
+    else:
+            db.insert_pinsuccess('no','no','no')
+            return render_template('graphical.html')
+
+    #insert this data to db   
+    return render_template('graphical.html') 
+
+@app.route('/graphicalForm1', methods=['POST'])
+def graphicalForm1():
+    username = request.form['username']
+    listOfImg = request.form.getlist('imgs');
+    listOfImg.sort()
+    finstring=' '.join(listOfImg)
+    print(finstring)
+    #push these names to db
+    if request.method == 'POST':
+        db.insert_graphicaldetails(username, finstring) 
+    return render_template('graphicalLogin.html')
+
+@app.route('/graphicalLoginForm1', methods=['POST'])
+def graphicalLoginForm1():
+    listOfImg = request.form.getlist('imgs')
+    attempt = 1
+    username = request.form['username']
+    listOfImg.sort()
+    finstring=' '.join(listOfImg)
+    exists = db.get_graphicaldetails(username,finstring)
+    print(exists)
+    if exists == 'true':
+            db.insert_graphicalsuccess('yes','na','na')
+            return render_template('musical.html') 
+    else:
+            return render_template('graphicalLogin2.html')
+    #push these names to db 
+    #retrieve previously inserted data and cross check; ensure to use "in op" so we wont have to worry about order
+    #if match then go to return render_template('musical.html') else the one below
+    return render_template('graphicalLogin2.html')
+
+@app.route('/graphicalLoginForm2', methods=['POST'])
+def graphicalLoginForm2():
+    listOfImg = request.form.getlist('imgs');
+    username = request.form['username']
+    attempt = 2
+    listOfImg.sort()
+    finstring=' '.join(listOfImg)
+    exists = db.get_graphicaldetails(username,finstring)
+    print(exists)
+    if exists == 'true':
+            db.insert_graphicalsuccess('no','yes','na')
+            return render_template('musical.html') 
+    else:
+            return render_template('graphicalLogin3.html')
+
+    #push these names to db 
+    #retrieve previously inserted data and cross check; ensure to use "in op" so we wont have to worry about order
+    #if match then go to return render_template('musical.html') else the one below
+    return render_template('graphicalLogin3.html')
+
+@app.route('/graphicalLoginForm3', methods=['POST'])
+def graphicalLoginForm3():
+    listOfImg = request.form.getlist('imgs');
+    username = request.form['username']
+    attempt = 3
+    #push these names to db 
+    listOfImg.sort()
+    finstring=' '.join(listOfImg)
+    exists = db.get_graphicaldetails(username,finstring)
+    print(exists)
+    if exists == 'true':
+            db.insert_graphicalsuccess('no','no','yes')
+            return render_template('musical.html') 
+    else:
+            db.insert_graphicalsuccess('no','no','no')
+            return render_template('musical.html')
+
+    return render_template('musical.html')
+
+@app.route('/musicalForm1', methods=['POST'])
+def musicalForm1():
+    musicName = request.form['music']
+    username = request.form['username']
+    #insert this value to db 
+    if request.method == 'POST':
+        db.insert_musicaldetails(username, musicName)    
+    return render_template('musicalLogin.html')
+
+@app.route('/musicalLoginForm1', methods=['POST'])
+def musicalLoginForm1():
+    musicName = request.form['music']
+    username = request.form['username']
+    attempt=1
+    #push these names to db 
+    #retrieve previously inserted data and cross check; 
+    #if match then go to return render_template('survey.html') else the one below
+    exists = db.get_musicaldetails(username,musicName)
+    if exists == 'true':
+            db.insert_musicalsuccess('yes','na','na')
+            return render_template('survey.html') 
+    else:
+            return render_template('musicalLogin2.html')
+    
+
+    
+
+@app.route('/musicalLoginForm2', methods=['POST'])
+def musicalLoginForm2():
+    musicName = request.form['music']
+    username = request.form['username']
+    attempt =2
+    #push these names to db 
+    #retrieve previously inserted data and cross check; 
+    #if match then go to return render_template('survey.html') else the one below
+    exists = db.get_musicaldetails(username,musicName)
+    print(exists)
+    if exists == 'true':
+            db.insert_musicalsuccess('no','yes','na')
+            return render_template('survey.html') 
+    else:
+            return render_template('musicalLogin3.html')
+
+
+@app.route('/musicalLoginForm3', methods=['POST'])
+def musicalLoginForm3():
+    musicName = request.form['music']
+    username = request.form['username']
+    attempt =3
+    #push these names to db 
+    exists = db.get_musicaldetails(username,musicName)
+    print(exists)
+    if exists == 'true':
+            db.insert_pinsuccess('no','no','no')
+            return render_template('survey.html') 
+    else:
+            return render_template('survey.html')
+    return render_template('survey.html')
+
+@app.route('/surveyQuestion', methods=['POST'])
+def surveyQuestion():
+    ansQ1 = request.form['q1']
+    ansQ2 = request.form['q2']
+    ansQ3 = request.form['q3']
+    ansQ4 = request.form['q4']
+    ansQ1Comm = request.form['pinComment1']
+    ansQ2Comm = request.form['pinComment2']
+    ansQ3Comm = request.form['pinComment3']
+    ansQ4Comm = request.form['pinComment4']
+    ansQ5Comm = request.form['pinComment5']
+    if request.method == 'POST':
+        db.insert_surdetails(ansQ1,ansQ2,ansQ3,ansQ4,ansQ1Comm,ansQ2Comm,ansQ3Comm,ansQ4Comm,ansQ5Comm)
+    return render_template('thankyou.html')
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
---
+++ db_create.py
@@ -0,0 +1,209 @@
+import pymysql
+
+conn = pymysql.connect(
+        host= 'database2.cwffazstjbiz.eu-west-1.rds.amazonaws.com', 
+        port = 3306,
+        user = 'admin', 
+        password = 'Vargheseoreilly123',
+        db = 'practicum',
+        
+        )
+
+#Table Creation
+# cursor=conn.cursor()
+# create_table="""
+# create table Consent (choice1 varchar(10),choice2 varchar(10),choice3 varchar(10),choice4 varchar(10),choice5 varchar(10),choice6 varchar(10) )
+
+# """
+# cursor.execute(create_table)
+
+#Table Creation
+# cursor=conn.cursor()
+# create_table="""
+# create table Questionnaire (q1 varchar(10),q2 varchar(10),q3 varchar(10),q4 varchar(10),q5a varchar(10),q5b varchar(10),q5c varchar(10),q5d varchar(10),pinComment5a varchar(300),pinComment5b varchar(300),pinComment5c varchar(300),pinComment5d varchar(300),q6 varchar(10),q7 varchar(10),pinComment6 varchar(300),pinComment7 varchar(300) )
+
+# """
+# cursor.execute(create_table)
+
+#Table Creation
+# cursor=conn.cursor()
+# create_table="""
+# create table Password (username varchar(20),password varchar(8) )
+
+# """
+# cursor.execute(create_table)
+
+# Table Creation
+# cursor=conn.cursor()
+# create_table="""
+# create table PasswordSuccess (firstattempt varchar(5),secondattempt varchar(5),thirdattempt varchar(5) )
+
+# """
+# cursor.execute(create_table)
+
+# Table Creation
+# cursor=conn.cursor()
+# create_table="""
+# create table Pin (username varchar(20),pin varchar(4) )
+
+# """
+# cursor.execute(create_table)
+
+# # Table Creation
+# cursor=conn.cursor()
+# create_table="""
+# create table PinSuccess (firstattempt varchar(5),secondattempt varchar(5),thirdattempt varchar(5) )
+
+# """
+# cursor.execute(create_table)
+
+# Table Creation
+# cursor=conn.cursor()
+# create_table="""
+# create table MusicalSuccess (firstattempt varchar(5),secondattempt varchar(5),thirdattempt varchar(5) )
+
+# """
+# cursor.execute(create_table)
+
+# #Table Creation
+# cursor=conn.cursor()
+# create_table="""
+# create table Musical (username varchar(20),music varchar(8) )
+
+# """
+# cursor.execute(create_table)
+
+#Table Creation
+# cursor=conn.cursor()
+# create_table="""
+# create table Survey (q1 varchar(10),q2 varchar(10),q3 varchar(10),q4 varchar(10), pinComment1 varchar(300),pinComment2 varchar(300),pinComment3 varchar(300),pinComment4 varchar(300),pinComment5 varchar(500) )
+
+# """
+# cursor.execute(create_table)
+
+#Table Creation
+# cursor=conn.cursor()
+# create_table="""
+# create table Graphical (username varchar(20), imgs varchar(50) )
+
+# """
+# cursor.execute(create_table)
+
+# Table Creation
+# cursor=conn.cursor()
+# create_table="""
+# create table GraphicalSuccess (firstattempt varchar(5),secondattempt varchar(5),thirdattempt varchar(5) )
+# """
+# cursor.execute(create_table)
+
+
+#insert query
+def insert_details(choice1,choice2,choice3,choice4,choice5,choice6):
+    cur=conn.cursor()
+    cur.execute("INSERT INTO Consent (choice1,choice2,choice3,choice4,choice5,choice6) VALUES (%s,%s,%s,%s,%s,%s)", (choice1,choice2,choice3,choice4,choice5,choice6))
+    conn.commit()
+
+#read the data
+def get_passdetails(username,password):
+    cur=conn.cursor()
+    cur.execute("SELECT *  FROM Password WHERE username = %s AND password = %s", (username, password))
+    details = cur.fetchone()
+    if details:
+    	return 'true'
+    else: 	
+    	return 'false'
+
+
+#insert query
+def insert_questdetails(ansQ1,ansQ2,ansQ3,ansQ4,ansQ5a,ansQ5b,ansQ5c,ansQ5d,ansQComm5a,ansQComm5b,ansQComm5c,ansQComm5d,ansQ6,ansQ7,ansQ6Comm,ansQ7Comm):
+    cur=conn.cursor()
+    cur.execute("INSERT INTO Questionnaire (q1,q2,q3,q4,q5a,q5b,q5c,q5d,pinComment5a,pinComment5b,pinComment5c,pinComment5d,q6,q7,pinComment6,pinComment7) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (ansQ1,ansQ2,ansQ3,ansQ4,ansQ5a,ansQ5b,ansQ5c,ansQ5d,ansQComm5a,ansQComm5b,ansQComm5c,ansQComm5d,ansQ6,ansQ7,ansQ6Comm,ansQ7Comm))
+    conn.commit()
+
+#insert query
+def insert_passdetails(username,password):
+    cur=conn.cursor()
+    cur.execute("INSERT INTO Password (username,password) VALUES (%s,%s)", (username,password))
+    conn.commit()
+
+def insert_passsuccess(firstattempt,secondattempt,thirdattempt):
+    cur=conn.cursor()
+    cur.execute("INSERT INTO PasswordSuccess (firstattempt,secondattempt,thirdattempt) VALUES (%s,%s,%s)", (firstattempt,secondattempt,thirdattempt))
+    conn.commit()
+
+
+#insert query
+def insert_pindetails(username,pin):
+    cur=conn.cursor()
+    cur.execute("INSERT INTO Pin (username,pin) VALUES (%s,%s)", (username,pin))
+    conn.commit()
+
+#read the data
+def get_pindetails(username,pin):
+    cur=conn.cursor()
+    cur.execute("SELECT *  FROM Pin WHERE username = %s AND pin = %s", (username, pin))
+    details = cur.fetchone()
+    if details:
+    	return 'true'
+    else: 	
+    	return 'false'
+
+
+def insert_pinsuccess(firstattempt,secondattempt,thirdattempt):
+    cur=conn.cursor()
+    cur.execute("INSERT INTO PinSuccess (firstattempt,secondattempt,thirdattempt) VALUES (%s,%s,%s)", (firstattempt,secondattempt,thirdattempt))
+    conn.commit()
+
+
+def insert_musicaldetails(username,musicName):
+    cur=conn.cursor()
+    cur.execute("INSERT INTO Musical (username,music) VALUES (%s,%s)", (username,musicName))
+    conn.commit()
+
+#read the data
+def get_musicaldetails(username,musicName):
+    cur=conn.cursor()
+    cur.execute("SELECT *  FROM Musical WHERE username = %s AND music = %s", (username, musicName))
+    details = cur.fetchone()
+    if details:
+        return 'true'
+    else:   
+        return 'false'
+
+
+def insert_musicalsuccess(firstattempt,secondattempt,thirdattempt):
+    cur=conn.cursor()
+    cur.execute("INSERT INTO MusicalSuccess (firstattempt,secondattempt,thirdattempt) VALUES (%s,%s,%s)", (firstattempt,secondattempt,thirdattempt))
+    conn.commit()
+
+#insert query
+def insert_surdetails(ansQ1,ansQ2,ansQ3,ansQ4,ansQ1Comm,ansQ2Comm,ansQ3Comm,ansQ4Comm,ansQ5Comm):
+    cur=conn.cursor()
+    cur.execute("INSERT INTO Survey (q1,q2,q3,q4,pinComment1,pinComment2,pinComment3,pinComment4,pinComment5) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)", (ansQ1,ansQ2,ansQ3,ansQ4,ansQ1Comm,ansQ2Comm,ansQ3Comm,ansQ4Comm,ansQ5Comm))
+    conn.commit()
+
+def insert_graphicaldetails(username,listOfImg):
+    cur=conn.cursor()
+    cur.execute("INSERT INTO Graphical (username,imgs) VALUES (%s,%s)", (username,listOfImg))
+    conn.commit()
+
+def get_graphicaldetails(username,listOfImg):
+    cur=conn.cursor()
+    cur.execute("SELECT *  FROM Graphical WHERE username = %s AND imgs = %s", (username, listOfImg))
+    details = cur.fetchone()
+    if details:
+        return 'true'
+    else:   
+        return 'false'
+
+def insert_graphicalsuccess(firstattempt,secondattempt,thirdattempt):
+    cur=conn.cursor()
+    cur.execute("INSERT INTO GraphicalSuccess (firstattempt,secondattempt,thirdattempt) VALUES (%s,%s,%s)", (firstattempt,secondattempt,thirdattempt))
+    conn.commit()
+
+
+
+
+
+
+
